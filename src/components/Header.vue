fb974eb420ccd1d8d9ace8edbb0ad183433d986a
PhysicistSouravDas <souravdas.sd20@gmail.com>
2020-09-23 19:31:48 +0530

    ✏️ Fixed Typo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <b-nav>
-            <b-nav-item diabled>Quizzy</b-nav-item>
+            <b-nav-item disabled>Quizzy</b-nav-item>
             <b-nav-item disabled>Counter: 4/10</b-nav-item>
         </b-nav>
     </div>
